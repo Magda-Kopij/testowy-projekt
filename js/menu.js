31e541f9e55689a4ec75c4b54d704b2c6038931a
Magda-Kopij <magda.kopij92@gmail.com>
2022-11-29 00:47:04 +0100

    dodanie js+menu - mobile

---
+++ js/menu.js
@@ -0,0 +1,11 @@
+const menuOverlay = document.querySelector(".mobile-menu");
+const closeMenuButton = document.querySelector(".menu-close-btn");
+const openMenuBtn = document.querySelector(".");
+
+openMenuBtn.addEventListener("click", (e) => {
+  menuOverlay.classList.remove("menu-hidden");
+});
+
+closeMenuButton.addEventListener("click", (e) => {
+  menuOverlay.classList.add("menu-hidden");
+});
