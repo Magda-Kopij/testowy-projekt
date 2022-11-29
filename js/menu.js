556b5d017373ae2b2681f8e41ff43eecd3e508bf
Magda-Kopij <magda.kopij92@gmail.com>
2022-11-29 22:03:59 +0100

    up

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,6 @@
-const menuOverlay = document.querySelector(".mobile-menu");
-const closeMenuButton = document.querySelector(".menu-close-btn");
-const openMenuBtn = document.querySelector(".");
+const menuOverlay = document.querySelector('.mobile-menu');
+const closeMenuButton = document.querySelector('.menu-close-btn');
+const openMenuBtn = document.querySelector('.menu-open-btn');
 
 openMenuBtn.addEventListener("click", (e) => {
   menuOverlay.classList.remove("menu-hidden");
